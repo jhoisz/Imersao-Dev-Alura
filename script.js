1046e41063971bc10ec928e45b6659d5cba2fba1
jhoisz <jvrda@ufpi.edu.br>
2022-09-02 09:34:12 -0300

    Aula 01 + desafios propostos

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+/*
+-----------------------
+| Imersão Dev 5        |
+| Aula 01              |
+| Calculadora de média |
+-----------------------
+*/
+
 var notaDoPrimeiroBimestre = 9
 var notaDoSegundoBimestre = 7
 var notaDoTerceiroBimestre = 4
@@ -7,31 +15,44 @@ var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiro
 var notaFixada = notaFinal.toFixed(1)
 console.log(notaFixada)
 
-//desafio 1:
+//DESAFIO 1: Separar o cálculo
 
-var somaDasNotas = notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre
+// var somaDasNotas = notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre
+// var notaFinal = somaDasNotas / 4
+// var notaFixada = notaFinal.toFixed(1)
 
-var notaFinal = somaDasNotas / 4
+// console.log(notaFixada)
 
-var notaFixada = notaFinal.toFixed(1)
+//DESAFIO 2: Usar condicionais para mostrar se foram aprovados
 
-console.log(notaFixada)
+// if (notaFinal >= 7) {
+//     console.log("Aprovado!")
+// } else {
+//     console.log("Reprovado!")
+// }
 
-//desafio 2:
+//DESAFIO 3: Conversão de Fahrenheit para Celsius
 
-if (notaFinal >= 7) {
-    console.log("Aprovado!")
-} else {
-    console.log("Reprovado!")
-}
+// console.log('Convertendo de Fahrenheit para Celsius:')
 
-//desafio 3:
-console.log('Convertendo de Fahrenheit para Celsius:')
-var tempFahrenheit = 70
+// var tempFahrenheit = 70
+// var tempCelsius = (tempFahrenheit - 32) / 1.8
 
-var tempCelsius = (tempFahrenheit - 32) / 1.8
-console.log(tempFahrenheit + 'ºF é ' + tempCelsius.toFixed(2) + 'ºC')
+// console.log(tempFahrenheit + 'ºF é ' + tempCelsius.toFixed(2) + 'ºC')
 
-// desafio 4:
+// DESAFIO 4: Fazer operações e imprimir a média no console.log
 
 console.log(((notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4).toFixed(1))
+
+// DESAFIO 5: Escrever na página e não no console
+
+// document.write("Sua nota é: " + notaFixada)
+//ou
+// alert("Sua nota é: " + notaFixada)
+//ou
+document.write("<h1>" + "Sua nota é: " + notaFixada + "</h1>")
+
+// DESAFIO 6: Mudar a cor do fundo e a imagem
+//Alterei em body (style.css):
+// background-color: blue;
+// background-image: url('https://images.unsplash.com/photo-1534644107580-3a4dbd494a95?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80');
