f1e126ef562e7e0742d0c98283f6d25068a5a071
jhoisz <jvrda@ufpi.edu.br>
2022-09-05 09:15:51 -0300

    Aula 02

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+/*
+-----------------------
+| Imersão Dev 5        |
+| Aula 02              |
+| Conversor de moead   |
+-----------------------
+*/
+
 function Converter() {
     var valorElemento = document.getElementById("valor");
     var valor = valorElemento.value;
@@ -11,3 +19,41 @@ function Converter() {
 
     elementoValorConvertido.innerHTML = valorConvertido;
 }
+
+
+//DESAFIO 1: Conversão de moedas
+// function ConveterRealParaEuro() {
+//     var valorElemento = document.getElementById("valor");
+//     var valor = valorElemento.value;
+//     var valorEmRealNumerico = parseFloat(valor);
+
+//     var valorEuro = 5.21;
+//     var valorEmReal = valorEmRealNumerico * valorEuro
+//     var elementoValorConvertido = document.getElementById("valorConvertido");
+//     var valorConvertido = "O resultado em real é R$" + valorEmReal;
+//     elementoValorConvertido.innerHTML = valorConvertido;
+// }
+
+//DESAFIO 2: Converter km para anos-luz
+
+// function ConverterKmParaAnosLuz() {
+//     var distanciaKm = parseFloat(document.getElementById("valor").value)
+//     var distanciaAnosLuz = distanciaKm * 1.057e-13
+
+//     var resultado = document.getElementById("valorConvertido")
+//     var valorConvertido = "KM para Anos luz: " + distanciaAnosLuz;
+//     resultado.innerHTML = valorConvertido
+// }
+
+
+// DESAFIO 3: Conversor de temperatura (Celsius para Kelvin e Fahrenheit)
+
+function ConversorDeCelcius() {
+    var tempCelcius = parseFloat(document.getElementById("valor").value)
+
+    var valorEmFah
+
+}
+
+
+
